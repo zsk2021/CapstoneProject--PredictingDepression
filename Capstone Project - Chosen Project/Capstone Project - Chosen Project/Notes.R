e0245361743f163f67c65743862357ec34477e58
zsk2021 <zsombor.szoke@yahoo.co.uk>
2022-05-09 21:53:44 +0100

    tidy format generated for group 1 and 2

---
+++ Capstone Project - Chosen Project/Capstone Project - Chosen Project/Notes.R
@@ -0,0 +1,9 @@
+# 1. semi_join() return all rows from x with a match in y . 
+# 2. anti_join() return all rows from x without a match in y .
+# 3. left_join() returns all of the rows from one table (the left side) and any 
+# matching rows from the second table. 
+# 4. rbind() combines several vectors, matrices and/or data frames by rows
+# maths in R: https://rpruim.github.io/s341/S19/from-class/MathinRmd.html
+# 5. sweep() function in R Language is used to apply the operation “+ or -” to 
+# the row or column in data matrix
+??qplot
